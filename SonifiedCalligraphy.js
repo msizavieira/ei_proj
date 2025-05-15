577291550938474fd77400d7c75c1777f212f216
msizavieira <fa66cd@student.aau.dk>
2025-05-16 00:42:18 +0200

    1st Project Commit

---
+++ SonifiedCalligraphy.js
@@ -0,0 +1,97 @@
+let synth;
+let isDrawing = false;
+let selectedMode = 'white';
+let clr = 'white';
+let lastTriggerTime = 0;
+let triggerInterval = 50; // milliseconds (adjust as needed)
+
+
+function setup() {
+  createCanvas(windowWidth/2, windowHeight/2);
+  background(0);
+  
+  let started = false;
+
+  if (!started) {
+    let startButton = createButton('Start Sound');
+    startButton.position(width / 2 - 50, height / 2);
+    startButton.mousePressed(() => {
+      Tone.start().then(() => {
+        started = true;
+        startButton.remove(); // Remove the button once it's clicked
+        console.log('Audio started');
+      });
+    });
+  }
+    
+  let redclr = createButton('Red');
+  redclr.position(0,0);
+  let greenclr = createButton('Green');
+  greenclr.position(40,0);
+  let whiteclr = createButton('White');
+  whiteclr.position(93,0);
+  let blueclr = createButton('Blue');
+  blueclr.position(143,0);
+  let clearButton = createButton("Clear");
+  clearButton.position(200, 0);
+  
+  greenclr.mousePressed(() => synthclrsetup('green'));
+  redclr.mousePressed(() => synthclrsetup('red'));
+  whiteclr.mousePressed(() => synthclrsetup('white'));
+  blueclr.mousePressed(() => synthclrsetup('blue'));
+  clearButton.mousePressed(() => {background(0)});
+
+  // Setup a basic synth
+  synth = new Tone.MonoSynth().toDestination();
+  
+}
+
+function synthclrsetup(clr) {
+  if (synth) synth.dispose();
+  
+  if (clr === 'red'){
+    synth = new Tone.FMSynth().toDestination();
+  }else
+  if (clr === 'green'){
+    synth = new Tone.DuoSynth().toDestination();
+  }else
+  if (clr === 'white'){
+    synth = new Tone.MonoSynth().toDestination();
+  }else
+  if (clr === 'blue') {
+    synth = new Tone.AMSynth().toDestination();
+  }
+  
+  selectedMode = clr;
+  console.log(clr);
+}
+
+function draw() {
+  if (isDrawing) {
+    // Draw path
+    stroke(selectedMode);
+    strokeWeight(2);
+    line(pmouseX, pmouseY, mouseX, mouseY);
+
+    // Calculate movement speed
+    let dx = mouseX - pmouseX;
+    let dy = mouseY - pmouseY;
+    let speed = sqrt(dx * dx + dy * dy);
+
+    // Map movement to sound parameters
+    let freq = map(mouseX, 0, width, 220, 880);
+    let velocity = map(speed, 0.01, 10, 0.01, 1.0);
+    velocity = constrain(velocity, 0.1, 1.0);
+    
+    synth.triggerAttackRelease(freq, 0.05, undefined, velocity);
+  }
+}
+
+function mousePressed() {
+  isDrawing = true;
+  Tone.start();
+}
+
+function mouseReleased() {
+  isDrawing = false;
+}
